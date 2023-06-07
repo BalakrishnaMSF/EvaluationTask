869e57600c9ef2f8712ab2a0cb0e52498c745575
balakrishna.g <balakrishna.s@marketsimplified.com>
2023-06-07 15:13:32 +0530

    Initial commit

---
+++ app/src/main/java/com/example/service/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.service;
+
+import android.content.Intent;import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    Button buttonStart, buttonStop;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        buttonStart = findViewById(R.id.buttonStart);
+        buttonStop = findViewById(R.id.buttonStop);
+
+        buttonStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startService(new Intent(MainActivity.this,MyService.class));
+                Toast.makeText(MainActivity.this, "Service Started", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+        buttonStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stopService(new Intent(MainActivity.this,MyService.class));
+                Toast.makeText(MainActivity.this, "Service Stopped", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+
+    }
+}
